3f2beeef93848bc42c1e77541e6d9c6591436f8e
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-03-29 17:15:26 -0300

    refactor: improved the navbar and nav items components.

--- src/components/navbar/NavItems.tsx
+++ src/components/navbar/NavItems.tsx
@@ -1,18 +1,41 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClick } from "@/hooks/useOnClick";
 
 export default function NavItems() {
     const navRef = useRef<HTMLDivElement | null>(null);
-
     const [activeIdx, setActiveIdx] = useState<null | number>(null);
-
     const isAnyOpen = activeIdx !== null;
 
+    const handleClose = () => {
+        setActiveIdx(null);
+    };
+
+    useOnClick({
+        ref: navRef,
+        handler: handleClose,
+    });
+
+    useEffect(() => {
+        const handler = (event: KeyboardEvent) => {
+            // closes the element on key "Escape" press.
+            if (event.key === "Escape") {
+                handleClose();
+            }
+        };
+
+        document.addEventListener("keydown", handler);
+
+        return () => {
+            document.removeEventListener("keydown", handler);
+        };
+    }, []);
+
     return (
-        <div className="flex gap-4 h-full">
+        <div className="flex gap-4 h-full" ref={navRef}>
             {PRODUCT_CATEGORIES.map((category, idx) => {
                 const handleOpen = () => {
                     if (activeIdx === idx) {
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,8 +2,12 @@ import Link from "next/link";
 import MaxWidthWrapper from "../widthWrapper/MaxWidthWrapper";
 import { Icons } from "../icons/Icons";
 import NavItems from "./NavItems";
+import { buttonVariants } from "../ui/button";
+import Cart from "../cards/Cart";
 
 export default function Navbar() {
+    const user = null;
+
     return (
         <nav className="sticky bg-white inset-x-0 top-0 h-16 z-50">
             <header className="relative bg-white">
@@ -18,6 +22,53 @@ export default function Navbar() {
                             </div>
                             <div className="hidden z-50 lg:block lg:self-stretch lg:ml-8">
                                 <NavItems />
+                            </div>
+                            <div className="flex items-center ml-auto">
+                                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                                    {!user && (
+                                        <Link
+                                            href="/sign-in"
+                                            className={buttonVariants({
+                                                variant: "ghost",
+                                            })}
+                                        >
+                                            Sign in
+                                        </Link>
+                                    )}
+                                    {!user && (
+                                        <span
+                                            className="bg-slate-200 h-6 w-px"
+                                            aria-hidden="true"
+                                        />
+                                    )}
+                                    {!user && (
+                                        <Link
+                                            href="/register"
+                                            className={buttonVariants({
+                                                variant: "ghost",
+                                            })}
+                                        >
+                                            Register
+                                        </Link>
+                                    )}
+                                    {user && (
+                                        <span
+                                            className="bg-slate-200 h-6 w-px"
+                                            aria-hidden="true"
+                                        />
+                                    )}
+                                    {!user && (
+                                        <div className="flex lg:ml-6">
+                                            <span
+                                                className="bg-slate-200 h-6 w-px"
+                                                aria-hidden="true"
+                                            />
+                                        </div>
+                                    )}
+                                    <div className="flow-root ml-4 lg:ml-6">
+                                        <Cart />
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
