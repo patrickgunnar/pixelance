b91699d06fd7c7539a1dfec873fbf189ab3bc314
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-04-17 12:37:18 -0300

    feat: users collection.

---
+++ src/collections/Users.ts
@@ -0,0 +1,31 @@
+import { CollectionConfig } from "payload/types";
+
+export const Users: CollectionConfig = {
+    slug: "users",
+    auth: true,
+    access: {
+        read: () => true,
+        create: () => true,
+    },
+    fields: [
+        {
+            name: "role",
+            required: true,
+            defaultValue: "user",
+            /*admin: {
+                condition: () => false,
+            },*/
+            type: "select",
+            options: [
+                {
+                    label: "Admin",
+                    value: "admin",
+                },
+                {
+                    label: "User",
+                    value: "user",
+                },
+            ],
+        },
+    ],
+};
