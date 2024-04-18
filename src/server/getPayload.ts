3ff78d73ffb52a0ffa3c586e271bae793363f59d
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-04-18 13:09:37 -0300

    feat: added the e-mail transporter.

--- src/server/getPayload.ts
+++ src/server/getPayload.ts
@@ -2,9 +2,20 @@ import dotenv from "dotenv";
 import path from "path";
 import type { InitOptions } from "payload/config";
 import payload, { Payload } from "payload";
+import nodemailer from "nodemailer";
 
 dotenv.config({
     path: path.resolve(__dirname, "..", "..", ".env"),
+});
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.resend.com",
+    secure: true,
+    port: 465,
+    auth: {
+        user: "resend",
+        pass: process.env.RESEND_API_KEY,
+    },
 });
 
 let cached = (global as any).payload;
@@ -20,7 +31,9 @@ interface Args {
     initOptions?: Partial<InitOptions>;
 }
 
-export async function getPayloadClient({ initOptions }: Args = {}): Promise<Payload> {
+export async function getPayloadClient({
+    initOptions,
+}: Args = {}): Promise<Payload> {
     if (!process.env.PAYLOAD_SECRET) {
         throw new Error("PAYLOAD_SECRET is missing");
     }
@@ -33,6 +46,11 @@ export async function getPayloadClient({ initOptions }: Args = {}): Promise<Payl
         cached.promise = payload.init({
             secret: process.env.PAYLOAD_SECRET,
             local: initOptions?.express ? false : true,
+            email: {
+                transport: transporter,
+                fromAddress: "onboarding@resend.dev",
+                fromName: "Pixelance",
+            },
             ...(initOptions || {}),
         });
     }
