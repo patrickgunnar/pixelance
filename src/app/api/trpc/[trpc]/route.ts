a75fd6fa6e823e2c807224abb264905fc3dda6da
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-04-18 13:08:24 -0300

    feat: added routers.

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,12 +1,13 @@
 import { appRouter } from "@/trpc";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
-function handler(req: Request) {
+const handler = (req: Request) => {
     fetchRequestHandler({
         endpoint: "/api/trpc",
-        router: appRouter,
-        createContext: () => ({}),
         req,
+        router: appRouter,
+        // @ts-expect-error context passed from express middleware
+        createContext: () => ({}),
     });
 }
 
