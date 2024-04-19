26fb1261bee76881448656ac57bf2a63c04324c8
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-04-19 19:49:34 -0300

    feat: added the products and media into the collections.

--- src/server/payload.config.ts
+++ src/server/payload.config.ts
@@ -1,10 +1,12 @@
-import { Users } from "../collections/Users";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import path from "path";
 import { buildConfig } from "payload/config";
 import dotenv from "dotenv";
+import { Users } from "../collections/Users";
+import { Products } from "../collections/products/Products";
+import { Media } from "../collections/Media";
 
 dotenv.config({
     path: path.resolve(__dirname, "..", "..", ".env"),
@@ -12,7 +14,7 @@ dotenv.config({
 
 export default buildConfig({
     serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-    collections: [Users],
+    collections: [Users, Products, Media],
     routes: {
         admin: "/sell",
     },
