85307f4539c786494ae0ef14364086297330e20c
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-04-25 11:52:21 -0300

    feat: added stripe handler to create and update products.

--- src/collections/products/Products.ts
+++ src/collections/products/Products.ts
@@ -1,10 +1,64 @@
 import { PRODUCT_CATEGORIES } from "../../config";
 import { CollectionConfig } from "payload/types";
+import { BeforeChangeHook } from "payload/dist/collections/config/types";
+import { Product } from "../../server/payload-types";
+import { stripe } from "../../lib/stripe";
+
+const addUser: BeforeChangeHook<Product> = async ({ req, data }) => {
+    const user = req.user;
+
+    return {
+        ...data,
+        user: user.id,
+    };
+};
 
 export const Products: CollectionConfig = {
     slug: "products",
     admin: {
         useAsTitle: "name",
+    },
+    hooks: {
+        beforeChange: [
+            addUser,
+            async (args) => {
+                if (args.operation === "create") {
+                    const data = args.data as Product;
+                    const createProduct = await stripe.products.create({
+                        name: data.name,
+                        default_price_data: {
+                            currency: "BRL",
+                            unit_amount: Math.round(data.price * 100),
+                        },
+                    });
+
+                    const updated: Product = {
+                        ...data,
+                        stripeId: createProduct.id,
+                        priceId: createProduct.default_price as string,
+                    };
+
+                    return updated;
+                } else if (args.operation === "update") {
+                    const data = args.data as Product;
+                    const updateProduct = await stripe.products.update(
+                        data.stripeId!,
+                        {
+                            name: data.name,
+                            default_price: data.priceId!,
+                        }
+                    );
+
+                    const updated: Product = {
+                        ...data,
+                        stripeId: updateProduct.id,
+                        priceId: updateProduct.default_price as string,
+                    };
+
+                    return updated;
+                }
+            },
+        ],
     },
     access: {},
     fields: [
