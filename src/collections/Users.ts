2b9f1fe13e357353472277a53e31e35dba501d24
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-04-29 10:16:40 -0300

    feat: added verify e-mail template and user's security.

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,23 +1,62 @@
-import { CollectionConfig } from "payload/types";
+import { Access, CollectionConfig } from "payload/types";
+import { PrimaryActionEmailHtml } from "../components/emails/PrimaryActionEmail";
+
+const adminsAndUsers: Access = ({ req: { user } }) => {
+    if (user.role === "admin") return true;
+
+    return {
+        id: {
+            equals: user.id,
+        },
+    };
+};
 
 export const Users: CollectionConfig = {
     slug: "users",
     auth: {
         verify: {
             generateEmailHTML: ({ token }) => {
-                return `
-                    <a href="${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}">
-                        Verify Account
-                    </a>
-                `;
+                const href = `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`;
+
+                return PrimaryActionEmailHtml({
+                    actionLabel: "Verifu your account",
+                    buttonText: "Verify Account",
+                    href,
+                });
             },
         },
     },
     access: {
-        read: () => true,
+        read: adminsAndUsers,
         create: () => true,
+        update: ({ req }) => req.user.role === "admin",
+        delete: ({ req }) => req.user.role === "admin",
+    },
+    admin: {
+        hidden: ({ user }) => user.role !== "admin",
+        defaultColumns: ["id"],
     },
     fields: [
+        {
+            name: "products",
+            type: "relationship",
+            relationTo: "products",
+            label: "Products",
+            hasMany: true,
+            admin: {
+                condition: () => false,
+            },
+        },
+        {
+            name: "product_files",
+            type: "relationship",
+            relationTo: "product_files",
+            label: "Products Files",
+            hasMany: true,
+            admin: {
+                condition: () => false,
+            },
+        },
         {
             name: "role",
             required: true,
