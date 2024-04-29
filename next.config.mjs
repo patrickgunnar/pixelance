1c3c95cc363bf6d9c31be678bd711be566ac73b9
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-04-29 11:38:54 -0300

    feat: added the images domain.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
+        /*remotePatterns: [
             {
                 hostname: "localhost",
                 pathname: "**",
                 port: "3000",
                 protocol: "http",
             },
-        ],
+        ],*/
+        domains: ["localhost", "https://pixelance-production.up.railway.app/"],
     },
 };
 
