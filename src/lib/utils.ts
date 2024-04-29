e31083da473532dfddc34c15a53bf25a89dda0cd
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-04-29 11:39:39 -0300

    feat: added metadata constructor.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,43 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+export function constructMetadata({
+    title = "Pixelance - Your Marketplace for Exceptional Assets",
+    description = "Pixelance is your marketplace for exceptional online assets",
+    image = "/thumbnail.png",
+    icons = "/favicon.ico",
+    noIndex = false,
+}: {
+    title?: string;
+    description?: string;
+    image?: string;
+    icons?: string;
+    noIndex?: boolean;
+} = {}): Metadata {
+    return {
+        title,
+        description,
+        openGraph: {
+            title,
+            description,
+            images: [
+                {
+                    url: image,
+                },
+            ],
+        },
+        icons,
+        metadataBase: new URL("https://pixelance-production.up.railway.app/"),
+        ...(noIndex && {
+            robots: {
+                index: false,
+                follow: false,
+            },
+        }),
+    };
 }
