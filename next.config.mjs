26133d383cc5b9fb568045adc6df4924938960fc
Patrick Gunnar <patrickgunnar.me@gmail.com>
2024-04-29 12:53:16 -0300

    fix: fixed the remotePatterns.

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        /*remotePatterns: [
+        remotePatterns: [
             {
                 hostname: "localhost",
-                pathname: "**",
-                port: "3000",
                 protocol: "http",
             },
-        ],*/
-        domains: ["localhost", "https://pixelance-production.up.railway.app/"],
+            {
+                hostname: "pixelance-production.up.railway.app",
+                protocol: "https",
+            },
+        ],
     },
 };
 
